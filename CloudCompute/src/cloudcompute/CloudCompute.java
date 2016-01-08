57988d26fd0e6bd09611de21730ac283466f2985
CadeBrown <brown.cade@gmail.com>
2016-01-07 23:14:01 -0500

    First commit

---
+++ CloudCompute/src/cloudcompute/CloudCompute.java
@@ -0,0 +1,18 @@
+/*
+ *  ChemicalDevelopment 2016
+ *  Cloud computing parallel tasks
+ */
+package cloudcompute;
+
+/**
+ *
+ * @author Cade Brown
+ */
+public class CloudCompute {
+    
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
